6b489c3eb1381219bd5a73a17635b6ad83319b87
unknown <shamsiyevalibek90@gmail.com>
2025-04-29 21:22:49 +0300

    islombek9

--- if07.py
+++ if07.py
@@ -1,16 +1,30 @@
 def main(temp):
     """
     Display the message according to the following temperature conditions given to you in Celsius:
-    Use the elif statments.
-    Temp<0: "Freezing"
+    Use the elif statements.
+    Temp < 0: "Freezing"
     Temp 1-10: "Very Cold"
     Temp 11-20: "Cold"
     Temp 21-30: "Normal"
     Temp 31-40: "Hot"
-    Temp >40: "Very Hot"
+    Temp > 40: "Very Hot"
     Args:
         temp: Temperature in Celsius.
     Returns:
         str: return answer.
     """
-    return
+    if temp < 0:
+        return "Freezing"
+    elif temp >= 1 and temp <= 10:
+        return "Very Cold"
+    elif temp >= 11 and temp <= 20:
+        return "Cold"
+    elif temp >= 21 and temp <= 30:
+        return "Normal"
+    elif temp >= 31 and temp <= 40:
+        return "Hot"
+    elif temp > 40:
+        return "Very Hot"
+    else:
+        return "No data"  # bu holat temp == 0 bo‘lsa, masalan
+print(main(68))
