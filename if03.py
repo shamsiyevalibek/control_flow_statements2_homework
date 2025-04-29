253a8fb98ef1687acbb4b0bdc6cbbf20cc83e08d
unknown <shamsiyevalibek90@gmail.com>
2025-04-29 21:05:55 +0300

    salom

--- if03.py
+++ if03.py
@@ -1,4 +1,4 @@
-def main(a,b,c):
+def main(a, b, c):
     """
     Determine the number between large and small.
     Args:
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if (a >= b and a <= c) or (a <= b and a >= c):
+        return a
+    elif (b >= a and b <= c) or (b <= a and b >= c):
+        return b
+    else:
+        return c
+print(main(5,8,9))
