b981f1baff1b881c9f4c36dd6486221c10ffa055
unknown <shamsiyevalibek90@gmail.com>
2025-04-29 20:53:43 +0300

    alham

--- if01.py
+++ if01.py
@@ -1,11 +1,8 @@
 def main(a,b,c):
-    """
-    Find the largest of the numbers.
-    Args:
-        a: First number.
-        b: Second number.
-        c: Third number.
-    Returns:
-        int: return answer.
-    """
-    return
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+print(max(1,9,4))
